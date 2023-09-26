05b80ff6f1ff41f95f22969805b7292a729edf33
Lukasz Romanek <lukasz.romanek@o2.pl>
2023-09-26 11:42:09 +0200

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,88 @@
+/* Burger menu */
+
+const activeMenu = () => {
+	const mainPage = document.querySelector('.main-page');
+
+	const menuBtn = document.querySelector('.hero__panel p');
+	const navigation = document.querySelector('.nav__menu');
+	const closeBtn = document.querySelector('.nav__close p');
+	const opacityBox = document.querySelector('.opacity-box');
+
+	/* Service navigation */
+
+	const removeActiveClasses = () => {
+		mainPage.classList.remove('main-page--active');
+	};
+
+	menuBtn.addEventListener('click', () => {
+		mainPage.classList.add('main-page--active');
+
+		if (mainPage.classList.contains('main-page--active'))
+			navigation.addEventListener('click', (e) => {
+				if (e.target.nodeName == 'A') {
+					removeActiveClasses();
+				}
+			});
+	});
+
+	closeBtn.addEventListener('click', () => {
+		removeActiveClasses();
+	});
+
+	opacityBox.addEventListener('click', () => {
+		removeActiveClasses();
+	});
+};
+
+/* Scroll scripts */
+
+const scrollEffect = () => {
+	const currentSection = window.scrollY;
+	const sections = document.querySelectorAll('section');
+	const heroPanel = document.querySelector('.hero__panel');
+	const scrollText = document.querySelector('.hero__scroll-text');
+
+	sections.forEach((section) => {
+		if (section.classList.contains('white-bg') && section.offsetTop <= currentSection + 50) {
+			heroPanel.classList.add('hero__panel--scroll');
+		} else if (!section.classList.contains('white-bg') && section.offsetTop <= currentSection + 50) {
+			heroPanel.classList.remove('hero__panel--scroll');
+		}
+	});
+
+	if (currentSection >= 15) {
+		scrollText.classList.add('hero__scroll-text--change');
+	} else {
+		scrollText.classList.remove('hero__scroll-text--change');
+	}
+
+	/* ScrollSpy */
+
+	const sectionsBox = [];
+	const menuItem = document.querySelectorAll('.nav__menu a');
+
+	sections.forEach((section) => {
+		if (window.scrollY <= section.offsetTop + section.offsetHeight - 50) {
+			sectionsBox.push(section);
+
+			const activeSection = document.querySelector(`[href*="${sectionsBox[0].id}"]`);
+
+			menuItem.forEach((item) => item.classList.remove('active'));
+
+			activeSection.classList.add('active');
+		}
+	});
+};
+
+/* Current year */
+
+const handleCurrentYear = () => {
+	const footerYear = document.querySelector('.footer-year');
+	const year = new Date().getFullYear();
+
+	footerYear.innerText = year;
+};
+
+window.addEventListener('scroll', scrollEffect);
+activeMenu();
+handleCurrentYear();
